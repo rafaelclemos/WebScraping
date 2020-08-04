5b0d7eda07f211647a5eb7742d2452e949f13167
Rafael Lemos <64116148+rafaelclemos@users.noreply.github.com>
2020-08-04 00:56:43 -0300

    Add files via upload

--- src/main/java/TesteWebScraping/TesteWebScraping.java
+++ src/main/java/TesteWebScraping/TesteWebScraping.java
@@ -12,22 +12,29 @@ import org.apache.commons.io.FileUtils;
 public class TesteWebScraping {
     
     public static void main(String[] args) throws IOException {
-        //Get para primeira página
+        
+        //Utilizando a biblioteca do "jsoup" e o método "get" para acessar a página principal. 
         String baseSite = "http://www.ans.gov.br";
         Document response = Jsoup.connect("http://www.ans.gov.br/prestadores/tiss-troca-de-informacao-de-saude-suplementar").get();
+        
+        //Através do código html da página referenciei/filtrei apenas o que tinha por objetivo.
         Element link = response.select(".alert a[href]").first();
         String linkTiss = link.attr("href");
         
+        //Concatenando as informações já coletadas.
         String linkTissFull = baseSite + linkTiss;
         
+        //Através do código html da página referenciei/filtrei apenas o que tinha por objetivo.
         Document response2 = Jsoup.connect(linkTissFull).get();
         Element link2 = response2.select("table a[href]").first();
         String linkTissPdf = link2.attr("href");
         
+        //Concatenando endereços
         String linkTissPdfFinal = baseSite + linkTissPdf;
         salvaPDF(linkTissPdfFinal);
     }
     
+    //Salvando o arquivo mais atual da tabela padrao tiss componente_organizaciona.
     public static void salvaPDF(String url) throws IOException {
         URL urlPdf = new URL(url);
         File destination = new File("linkTiss.pdf");
